943174a2b66e84eea8e6b4c045fcff2af7917627
Elon Must <barissenyerli@gmail.com>
2020-10-23 11:10:01 +0300

    Add files via upload

--- fontCssWriter.py
+++ fontCssWriter.py
@@ -1,12 +1,25 @@
 import os
 def fontsAreReadyForFlask(fontsFolder):
+    """
+        Your project folder must be like
+        >Project-Folder
+        > > static
+        > >|> > fonts
+        > >|> >|> fontCssWriter.py -> run it in current folder
+        > >|> > css
+
+        When writing path way:
+            Additionally use / 
+            Don't use backslash \
+    """
     entries = os.listdir(fontsFolder)
     css = ""
     for entry in entries:
-        css+="@font-face {{font-family: {};src: url(\"".format(entry[:-4:])+"{{ "+"url_for('static', filename='{}')".format(entry)+' }}\"'+");}\n"
-        #print(css)
+        css+="@font-face {{font-family: {};src: url(\"".format(entry[:-4:])+"/static/fonts/{}".format(entry)+'\"'+");}\n"
+        #print(css) 
 
-    with open(fontsFolder+'/font.css','w') as f:
+    with open(fontsFolder[:-6:]+'/css/font.css','w') as f:
         f.write(css)
     print("It is Ready!")
 
+fontsAreReadyForFlask("C:/Users/<user>/Desktop/Project-Folder/static/fonts")
